773c3f096ba4b067d885fc31640bb05a1191ad7a
Ryan Zillini <nyrtrading134@gmail.com>
2023-07-17 10:17:00 -0400

    completed MVP

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,35 @@
 // add middlewares here related to actions
+const Action = require("./actions-model");
+const Project = require("../projects/projects-model");
+
+async function validateActionsId(req, res, next) {
+  try {
+    const { id } = req.params;
+    const action = await Action.get(id);
+    if (!action) {
+      res.status(404).json({ message: `Action with id: ${id} not found` });
+    } else {
+      req.action = action;
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+}
+
+function validateActionPost(req, res, next) {
+  const { notes, description, project_id } = req.body;
+  if (!notes || !description || !project_id) {
+    res.status(400).json({ message: "Please povide all required fields" });
+  } else {
+    req.notes = notes;
+    req.description = description;
+    req.project_id = project_id;
+    next();
+  }
+}
+
+module.exports = {
+  validateActionsId,
+  validateActionPost,
+};
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -5,12 +5,59 @@ const Actions = require("./actions-model");
 
 const router = express.Router();
 
+const {
+  validateActionsId,
+  validateActionPost,
+} = require("./actions-middlware");
+
 router.get("/", (req, res, next) => {
   Actions.get(req.params.id)
     .then((actions) => {
       res.status(200).json(actions);
     })
     .catch(next);
+});
+
+router.get("/:id", validateActionsId, (req, res) => {
+  res.json(req.action);
+});
+
+router.post("/", validateActionPost, (req, res, next) => {
+  Actions.insert({
+    description: req.body.description,
+    notes: req.body.notes,
+    project_id: req.body.project_id,
+  })
+    .then((action) => {
+      res.json(action);
+    })
+    .catch(next);
+});
+
+router.put("/:id", validateActionsId, validateActionPost, (req, res, next) => {
+  const { completed } = req.body;
+  if (!completed) {
+    res.status(400).json({ message: "please provide all required fields" });
+  }
+  Actions.update(req.params.id, {
+    description: req.description,
+    notes: req.notes,
+    project_id: req.project_id,
+    completed: req.body.completed,
+  })
+    .then((action) => {
+      res.json(action);
+    })
+    .catch(next);
+});
+
+router.delete("/:id", validateActionsId, async (req, res, next) => {
+  try {
+    await Actions.remove(req.params.id);
+    res.json(req.action);
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
